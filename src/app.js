ba092a9a8e1632256636eb135d166eba903291b2
Merritt Baggett <mertbagt@gmail.com>
2021-01-28 21:17:52 -0500

    debugging

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 require('dotenv').config()
-
-const DaysService = require('./days/days-service')
-
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
-// const foldersRouter = require('./folders/folders-router')
-// const notesRouter = require('./notes/notes-router')
 const daysRouter = require('./days/days-router')
 const dishesRouter = require('./dishes/dishes-router')
 const assignmentsRouter = require('./assignments/assignments-router')
@@ -24,9 +19,7 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-// app.use('/folders', foldersRouter)
-// app.use('/notes', notesRouter)
-// app.use('/days', daysRouter)
+app.use('/days', daysRouter)
 app.use('/dishes', dishesRouter)
 app.use('/assignments', assignmentsRouter)
 
@@ -34,19 +27,11 @@ app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
-app.get('/days', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  DaysService.getAllDays(knexInstance)
-  .then(days => {
-    res.json(days.map(serializeDay))
-  })
-  .catch(next)
-})
-
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-      response = { error: { message: 'server error' } }
+//      response = { error: { message: 'server error' } }
+      response = { message: error.message, error }
     } else {
       console.error(error)
       response = { message: error.message, error }
